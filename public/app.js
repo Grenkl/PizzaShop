34219a5e71b3965a5bfbdcc74bdffeeadb45d738
Grenkl <s2oop@yandex.ru>
2015-11-13 14:34:21 +0300

    Replace cart cantents on cancel

--- public/app.js
+++ public/app.js
@@ -72,11 +72,12 @@ function cart_get_orders()
 	return orders;
 }
 
-function cansel_order()
+function cancel_order()
 {
 	window.localStorage.clear(); 
 	update_orders_input();
 	update_orders_button();
+	$('#cart').text('Your cart is now empty');
 	return false;
 
 }
